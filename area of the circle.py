add734506a908d76716cd0107666bb532ac57317
Nady123 <92248630+Nady123@users.noreply.github.com>
2021-10-10 14:36:59 +0530

    Update area of the circle.py
    This code prints the 
    1.Area of the circle
    2.Extension of the file

--- area of the circle.py
+++ area of the circle.py
@@ -1,5 +1,19 @@
+Task 1 
+
 import math
 radius = float(input("Enter the radius of the circle : "))
 
 area = math.pi*radius*radius
 print("Area of the circle is : {0}".format(area))
+
+Task 2
+
+filename = input("Input filename : ")
+f_extns = filename.split(".")
+print("extension of file is:"+repr(f_extns[-1]))
+
+
+
+
+
+
